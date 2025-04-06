ffe9f70d348bc5b02bf68ec8d1c863c0a49b9304
Shams Ali <61931686+shamxali@users.noreply.github.com>
2025-04-07 01:52:41 +0500

    ai-writer output page improved

--- lib/modules/ai_writer/pages/ai_writer_output_page.dart
+++ lib/modules/ai_writer/pages/ai_writer_output_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mywords/common/components/primary_button.dart';
-import 'package:mywords/common/widgets/ai_text_field.dart';
 
-import 'package:mywords/common/widgets/labeled_icons_row.dart';
 import 'package:mywords/common/widgets/step_indicator_widget.dart';
 import 'package:mywords/constants/app_colors.dart';
 import 'package:mywords/modules/ai_writer/cubit/ai_writer_cubit.dart';
@@ -39,17 +38,6 @@ class _AiWriterOutputPageState extends State<AiWriterOutputPage> {
                 ),
               ),
             ),
-            bottomNavigationBar: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-              padding: EdgeInsets.only(bottom: hasBottomSafeArea ? bottomPadding : 30),
-              child: PrimaryButton.filled(
-                onTap: () {},
-                title: 'Continue',
-                textColor: context.colorScheme.primary,
-                backgroundColor: Color(0xffD24DEE).withOpacity(0.15),
-                fontWeight: FontWeight.w700,
-              ),
-            ),
             body: Column(
               children: [
                 StepIndicator(activeSteps: [1, 2, 3]),
@@ -73,27 +61,53 @@ class _AiWriterOutputPageState extends State<AiWriterOutputPage> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  _TextFieldHeader(wordCount: state.wordCount),
-                                  AiTextField(
-                                    onChanged: (nextValue) {
-                                      context.read<AiWriterCubit>().updateText(nextValue);
-                                    },
-                                    textEditingController: aiWriterController,
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16.0),
+                                    child: Text(
+                                      'Output',
+                                      style: context.textTheme.titleLarge?.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                        color: context.colorScheme.onSurface,
+                                      ),
+                                    ),
                                   ),
-                                  LabeledIconsRow(
-                                    onSampleTextCallback: () {},
-                                    onUploadFileCallBack: () {},
-                                    onPasteTextCallBack: () {},
+                                  Divider(
+                                    color: Color(0xffDADADA),
+                                    height: 0,
                                   ),
-                                  SizedBox(height: 14),
+                                  Container(
+                                    width: double.infinity,
+                                    height: 300,
+                                    padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                                    child: SingleChildScrollView(
+                                        child: Padding(
+                                      padding: const EdgeInsets.only(bottom: 16.0),
+                                      child: Text(
+                                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                                        style: context.textTheme.titleMedium,
+                                      ),
+                                    )),
+                                    decoration: BoxDecoration(),
+                                  ),
                                 ],
                               ),
                             );
                           },
                         ),
                       )),
-                )
+                ),
+                SizedBox(height: 16),
+                _InfoStaticWidget(),
               ],
+            ),
+            bottomNavigationBar: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: EdgeInsets.only(bottom: hasBottomSafeArea ? bottomPadding : 30),
+              child: PrimaryButton.gradient(
+                onTap: () {},
+                title: 'Humanize Text',
+                fontWeight: FontWeight.w700,
+              ),
             ),
           );
         },
@@ -102,42 +116,114 @@ class _AiWriterOutputPageState extends State<AiWriterOutputPage> {
   }
 }
 
-class _TextFieldHeader extends StatelessWidget {
-  const _TextFieldHeader({super.key, required this.wordCount});
-
-  final int wordCount;
+class _InfoStaticWidget extends StatelessWidget {
+  const _InfoStaticWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text(
-                'Input',
-                style: context.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w700, color: context.colorScheme.onSurface),
-              ),
-              Spacer(),
-              Text.rich(
-                TextSpan(text: '${wordCount}', style: context.textTheme.bodySmall?.copyWith(color: AppColors.orange), children: [
-                  TextSpan(
-                    text: '/800 Words',
-                    style: context.textTheme.bodySmall?.copyWith(color: context.colorScheme.onSurface),
-                  )
-                ]),
-              ),
-            ],
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: Color(0xffDADADA),
           ),
-          SizedBox(height: 14),
-          Text(
-            'Please briefly describe your prompt *',
-            style: context.textTheme.bodySmall?.copyWith(color: context.colorScheme.onSurface, height: 1.5),
-          ),
-        ],
-      ),
-    );
+        ),
+        child: BlocConsumer<AiWriterCubit, AiWriterState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          builder: (context, state) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        '100%',
+                        style: context.textTheme.titleLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xffFF3D00),
+                        ),
+                      ),
+                      SizedBox(width: 30),
+                      Text.rich(
+                        TextSpan(
+                          text: 'Your Text is likely to be written',
+                          children: [
+                            TextSpan(
+                              text: ' by AI\n9/9 ',
+                              style: context.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400, color: Color(0xffFC5A5A)),
+                              children: [
+                                TextSpan(
+                                  text: 'sentences AI generated',
+                                  style: context.textTheme.titleMedium?.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        style: context.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Color(0xffDADADA),
+                  height: 0,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Third Party AI Scores',
+                        style: context.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 12),
+                      Row(
+                        children: [
+                          SvgPicture.asset('assets/images/svg/ic_human.svg'),
+                          SizedBox(width: 7),
+                          Text(
+                            'Human',
+                            style: context.textTheme.titleSmall
+                                ?.copyWith(fontWeight: FontWeight.w600, color: Color(0xff45C646), letterSpacing: 0, wordSpacing: 0),
+                          ),
+                          SizedBox(width: 15),
+                          SvgPicture.asset('assets/images/svg/ic_likely_ai.svg'),
+                          SizedBox(width: 7),
+                          Text(
+                            'Likely AI',
+                            style: context.textTheme.titleSmall
+                                ?.copyWith(fontWeight: FontWeight.w600, color: Color(0xffFABB18), letterSpacing: 0, wordSpacing: 0),
+                          ),
+                          SizedBox(width: 15),
+                          SvgPicture.asset('assets/images/svg/ic_ai_generated.svg'),
+                          SizedBox(width: 7),
+                          Text(
+                            'AI',
+                            style: context.textTheme.titleSmall?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xffFC5A5A),
+                              letterSpacing: 0,
+                              wordSpacing: 0,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
