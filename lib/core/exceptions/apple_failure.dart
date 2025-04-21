d402ac09d7873608db397abc087396403a19ba22
Shams Ali <61931686+shamxali@users.noreply.github.com>
2025-04-22 03:02:08 +0500

    feat(social-auth): add basic code

---
+++ lib/core/exceptions/apple_failure.dart
@@ -0,0 +1,49 @@
+class LogInWithAppleFailure implements Exception {
+  /// The associated error message.
+  final String message;
+
+  const LogInWithAppleFailure([
+    this.message = 'An unknown exception occurred.',
+  ]);
+
+  /// Create an authentication message
+  /// from a firebase authentication exception code.
+  factory LogInWithAppleFailure.fromCode(String code) {
+    switch (code) {
+      case 'account-exists-with-different-credential':
+        return const LogInWithAppleFailure(
+          'Account exists with different credentials.',
+        );
+      case 'invalid-credential':
+        return const LogInWithAppleFailure(
+          'The credential received is malformed or has expired.',
+        );
+      case 'operation-not-allowed':
+        return const LogInWithAppleFailure(
+          'Operation is not allowed.  Please contact support.',
+        );
+      case 'user-disabled':
+        return const LogInWithAppleFailure(
+          'This user has been disabled. Please contact support for help.',
+        );
+      case 'user-not-found':
+        return const LogInWithAppleFailure(
+          'Email is not found, please create an account.',
+        );
+      case 'wrong-password':
+        return const LogInWithAppleFailure(
+          'Incorrect password, please try again.',
+        );
+      case 'invalid-verification-code':
+        return const LogInWithAppleFailure(
+          'The credential verification code received is invalid.',
+        );
+      case 'invalid-verification-id':
+        return const LogInWithAppleFailure(
+          'The credential verification ID received is invalid.',
+        );
+      default:
+        return const LogInWithAppleFailure();
+    }
+  }
+}
---
+++ lib/modules/authentication/repository/social_auth_repository.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mywords/core/exceptions/apple_failure.dart';
+import 'package:mywords/core/exceptions/google_failure.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+class SocialAuthRepository {
+  SocialAuthRepository({
+    FirebaseAuth? firebaseAuth,
+    GoogleSignIn? googleSignIn,
+  })  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
+
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  /// =================================== Google ===================================
+  Future<UserCredential> loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await _firebaseAuth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      throw LogInWithGoogleFailure.fromCode(e.code);
+    } catch (e) {
+      throw LogInWithGoogleFailure(e.toString());
+    }
+  }
+
+  /// =================================== Apple ===================================
+
+  Future<void> loginWithApple() async {
+    try {
+      // final appleProvider = AppleAuthProvider();
+      // await _firebaseAuth.signInWithProvider(appleProvider);
+
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      final oAuthProvider = OAuthProvider('apple.com');
+      final credential = oAuthProvider.credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+      await _firebaseAuth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      print('Apple error :: ${e.toString()}');
+      throw LogInWithAppleFailure.fromCode(e.code);
+    } catch (e) {
+      print('Apple error :: ${e.toString()}');
+      throw const LogInWithAppleFailure();
+    }
+  }
+}
