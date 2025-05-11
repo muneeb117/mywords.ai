9115b0b23c5d3039d845ab976c2a2382e9d77054
Shams Ali <61931686+shamxali@users.noreply.github.com>
2025-05-11 14:19:17 +0500

    fix: pop the feedback sheet on user action

--- lib/common/widgets/feeback_bottom_sheet.dart
+++ lib/common/widgets/feeback_bottom_sheet.dart
@@ -88,6 +88,7 @@ void showFeedbackBottomSheet(BuildContext context) {
 
                       final feedback = controller.text.trim();
                       Urls.sendFeedbackEmail(context, feedback);
+                      Navigator.pop(context);
                     },
                     title: 'Submit',
                   ),
