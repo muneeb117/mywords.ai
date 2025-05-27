df15be5ee53a377597fd8320d6cd566ceb204d73
muneeb011 <muhammadmuneeb117@gmail.com>
2025-05-27 23:57:52 +0500

    feat: implement resend otp

--- lib/modules/startup/widgets/splash_widget.dart
+++ lib/modules/startup/widgets/splash_widget.dart
@@ -12,12 +12,18 @@ class SplashWidget extends StatelessWidget {
     return Container(
       width: screenSize.width,
       height: screenSize.height,
-      color: const Color(0xff601FBE),
+      color: const Color(0xffffffff),
       child: Stack(
         children: [
-          Positioned(right: 0, top: paddingTop + 27, child: SvgPicture.asset('assets/images/svg/ic_splash_circle_right.svg', height: 170)),
-          Positioned(left: 0, top: paddingTop + 130, child: SvgPicture.asset('assets/images/svg/ic_splash_circle_left.svg', height: 170)),
-          Center(child: Image.asset('assets/images/png/img_splash.png', height: 110, width: 145)),
+          // Positioned(right: 0, top: paddingTop + 27, child: SvgPicture.asset('assets/images/svg/ic_splash_circle_right.svg', height: 170)),
+          // Positioned(left: 0, top: paddingTop + 130, child: SvgPicture.asset('assets/images/svg/ic_splash_circle_left.svg', height: 170)),
+          Center(
+            child: Image.asset(
+              'assets/images/png/img_splash.png',
+              height: 110,
+              width: 145,
+            ),
+          ),
         ],
       ),
     );
